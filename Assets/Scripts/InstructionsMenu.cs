913ae32ae0651f9667ae875d1675af7ecfb1c411
Tristan Tran <trantm@usc.edu>
2023-11-07 22:54:36 -0800

    Working New UI.. Needs icon change

---
+++ Assets/Scripts/InstructionsMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject scoreCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)){
+            if (isPaused){
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume (){
+      pauseMenuUI.SetActive(false);
+      scoreCanvas.SetActive(true);
+      Time.timeScale = 1f;
+      isPaused = false;
+    }
+
+    void Pause (){
+      scoreCanvas.SetActive(false);
+      pauseMenuUI.SetActive(true);
+      Time.timeScale = 0f;
+      isPaused = true;
+    }
+}
