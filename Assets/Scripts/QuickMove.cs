078472625cc64274b02037eef305559a51e6e523
Nicholas-Chu <nick.chu75@gmail.com>
2023-11-28 18:49:56 -0800

    quickmove visual indicator

--- Assets/Scripts/QuickMove.cs
+++ Assets/Scripts/QuickMove.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class QuickMove : PowerUp
 {
     private float speedIncrease = 7.0f;
+
     public QuickMove() : base("Quick Move", 10.0f)
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
     {
 
     }
@@ -19,10 +14,24 @@ public class QuickMove : PowerUp
     public override void Activate(Player player)
     {
         player.speed += speedIncrease;
+
+        // Get the TrailRenderer component from the player and enable it
+        TrailRenderer playerTrail = player.GetComponentInChildren<TrailRenderer>();
+        if (playerTrail != null)
+        {
+            playerTrail.enabled = true;
+        }
     }
 
     public override void Deactivate(Player player)
     {
         player.speed -= speedIncrease;
+
+        // Get the TrailRenderer component from the player and disable it
+        TrailRenderer playerTrail = player.GetComponentInChildren<TrailRenderer>();
+        if (playerTrail != null)
+        {
+            playerTrail.enabled = false;
+        }
     }
 }
